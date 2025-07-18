27d379ce3d4e300388626adcaf17dba917609d41
wss <528890686@qq.com>
2025-07-18 09:04:22 +0800

    Formily 封装实践

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Form3 from "./components/form3";
 import Form4 from "./components/form4";
 import FormCSZ from "./components/form_csz";
 import FormSelectInput from "./components/form_select_input";
-import FormCSZFilter from "./components/form_csz_filter";
+// import FormCSZFilter from "./components/form_csz_filter";
 
 function App() {
     return (
@@ -20,8 +20,8 @@ function App() {
             <FormCSZ/>
             <h1>Form4</h1>
             <Form4/>
-            <h1>FormCSZFilter</h1>
-            <FormCSZFilter/>
+            {/*<h1>FormCSZFilter</h1>*/}
+            {/*<FormCSZFilter/>*/}
             <h1>Form 选择框</h1>
             <FormSelectInput/>
             <h1>Form 表单筛选 JSON Schema 模式</h1>
